3f39063db459aec0a0682037bf5e8392a7955e21
BienFabiHSLU <fabian.bienz@stud.hslu.ch>
2020-11-29 16:22:29 +0100

    [MOD] Ausertungsskript

--- PAIND/Auswertungsskript.py
+++ PAIND/Auswertungsskript.py
@@ -7,8 +7,20 @@ Created on Fri Nov 27 22:22:59 2020
 import numpy as np
 import pandas as pd
 
+print ("Base Folder Path")
+baseFolderPath= input()
+print("File Imu Above")
+fileImuAbove = input()
+print("File Imu Bottom")
+fileImuBottom = input()
+print("File Leica Above")
+fileLeicaAbove = input()
+print("File Leica Bottom")
+fileLeicaBottom = input()
+
 ##Einlesen: Prisma Position oben
-leicaPosAbove = pd.read_csv(r"C:\Users\Simon Bienz\Documents\Fabian\HSLU\HS20\PAIND\Messdaten\test.txt")
+#leicaPosAbove = pd.read_csv(r"C:\Users\Simon Bienz\Documents\Fabian\HSLU\HS20\PAIND\Messdaten\test.txt")
+leicaPosAbove = pd.read_csv(baseFolderPath+"/"+fileLeicaAbove)
 aboveX=leicaPosAbove["field.point.x"].mean()
 aboveY=leicaPosAbove["field.point.y"].mean()
 aboveZ=leicaPosAbove["field.point.z"].mean()
@@ -23,7 +35,8 @@ aboveM=[aboveX,aboveY, aboveZ]
 #bottomZ=aboveZ-600.0
 
 ##Einlesen: Prisma Position unten
-leicaPosBottom = pd.read_csv(r"C:\Users\Simon Bienz\Documents\Fabian\HSLU\HS20\PAIND\Messdaten\test.txt")
+#leicaPosBottom = pd.read_csv(r"C:\Users\Simon Bienz\Documents\Fabian\HSLU\HS20\PAIND\Messdaten\test.txt")
+leicaPosBottom = pd.read_csv(baseFolderPath+"/"+fileLeicaBottom)
 bottomX=leicaPosBottom["field.point.x"].mean()
 bottomY=leicaPosBottom["field.point.y"].mean()
 bottomZ=leicaPosBottom["field.point.z"].mean()
@@ -33,20 +46,20 @@ bottomM=[bottomX, bottomY, bottomZ]
 
 
 #Einlesen: IMU-Daten oben
-imuAngle = pd.read_csv(r"C:\Users\Simon Bienz\Documents\Fabian\HSLU\HS20\PAIND\Messdaten\test.txt")
-
-aboveRollX=imuAngle["field.angular_velocity.z"]
-abovePitchY=imuAngle["field.angular_velocity.y"]
-aboveYawZ=imuAngle["field.angular_velocity.x"]
+#imuAngleAbove = pd.read_csv(r"C:\Users\Simon Bienz\Documents\Fabian\HSLU\HS20\PAIND\Messdaten\test.txt")
+imuAngleAbove = pd.read_csv(baseFolderPath+"/"+fileImuAbove)
+aboveRollX=imuAngleAbove["field.angular_velocity.z"]
+abovePitchY=imuAngleAbove["field.angular_velocity.y"]
+aboveYawZ=imuAngleAbove["field.angular_velocity.x"]
 aboveAngle=[aboveRollX, abovePitchY, aboveYawZ]
 #print(aboveAngle)
 
 #Einlesen: IMU-Daten unten
-imuAngle = pd.read_csv(r"C:\Users\Simon Bienz\Documents\Fabian\HSLU\HS20\PAIND\Messdaten\test.txt")
-
-bottomRollX=imuAngle["field.angular_velocity.z"]
-bottomPitchY=imuAngle["field.angular_velocity.y"]
-bottomYawZ=imuAngle["field.angular_velocity.x"]
+#imuAngleBottom = pd.read_csv(r"C:\Users\Simon Bienz\Documents\Fabian\HSLU\HS20\PAIND\Messdaten\test.txt")
+imuAngleBottom = pd.read_csv(baseFolderPath+"/"+fileImuBottom)
+bottomRollX=imuAngleBottom["field.angular_velocity.z"]
+bottomPitchY=imuAngleBottom["field.angular_velocity.y"]
+bottomYawZ=imuAngleBottom["field.angular_velocity.x"]
 bottomAngle=[bottomRollX, bottomPitchY, bottomYawZ]
 #print(bottomAngle)
 
@@ -74,13 +87,18 @@ aboveYawZmed=aboveYawZ.median()
 aboveAngleMedian=[aboveRollXmed, abovePitchYmed, aboveYawZmed]
 
 #Mittelwert der beiden Mediane, falls leiche Positionsänderung beim Verschieben
-meanRollX=np.mean(aboveRollXmed,bottomRollXmed)
-meanPitchY=np.mean(abovePitchYmed,bottomPitchYmed)
-meanYaw=np.mean(aboveYawZmed,bottomYawZmed)
+meanRollX=np.mean([aboveRollXmed,bottomRollXmed])
+meanPitchY=np.mean([abovePitchYmed,bottomPitchYmed])
+meanYaw=np.mean([aboveYawZmed,bottomYawZmed])
 
 meanAngleImu=[meanRollX, meanPitchY, meanYaw]
 
 #Gesamtwinkel des IMU: Achtung! Winkel sind bereits in Degree, zuerst wieder in RAd umrechnen!
 alphaRadImu=np.arccos(np.cos(np.radians(meanRollX))*np.cos(np.radians(meanPitchY)))
 alphaDegImu=np.degrees(alphaRadImu)
+
+
+#Ausgabe
+print("Gesamt-Neigungswinkel IMU\n"+str(alphaDegImu))
+print("Gesamt-Neigungswinkel LEICA\n" +str(alphaDegLeica))
 
